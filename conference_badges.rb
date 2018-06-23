a36626b8bd0d66d37ae1c844c9ef13bfa973c052
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 22:09:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,12 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array_of_names)
+  
   name_tag_array = Array.new
   counter=0
+  
   array_of_names.each do |person|
+    name_tag_array[counter]=
     
     
 end
